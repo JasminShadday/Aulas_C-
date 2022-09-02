26e04f1a9c1a2ed5851d9695302b88113cd3be20
JasminShadday <112671781+JasminShadday@users.noreply.github.com>
2022-09-01 22:19:35 -0300

    Add files via upload

---
+++ JSEx1a2508/JSEx1a2508/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using JSEx1a2508.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSEx1a2508.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.nome = "shadday";
+            home.email = "shaddayjasmim@gmail.com";
+            home.telefone = 956024311;
+            home.dataAdmissao = 223456;
+            home.salario = 12.4536;
+
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JSEx1a2508/JSEx1a2508/Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿namespace JSEx1a2508.Models
+{
+    public class HomeModel
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+        public int telefone { get; set; }
+        public int id { get; set;}
+        public double salario { get; set; }
+        public int dataAdmissao { get; set; }
+    }
+    
+    
+}
